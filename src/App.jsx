1867c8d39e13605cdeca4b9e44eebc5dc2c2c74b
LonerStyles <163217333+LonerStyles@users.noreply.github.com>
2026-03-08 19:09:15 -0500

    Corección Carta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Carta from "./views/Carta";
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Carta></Carta>
       <Footer></Footer>
     </>
   );
--- src/components/Plato.jsx
+++ src/components/Plato.jsx
@@ -1,8 +1,12 @@
-export default function Plato({name, desc, price}) {
+import "../styles/Plato.css";
+
+export default function Plato({ name, desc, price }) {
   return (
     <div className="plato">
-      <h2 className="food-name">{name}</h2>
-      <p className="desc">{desc}</p>
+      <div className="plato1">
+        <h2 className="food-name">{name}</h2>
+        <p className="desc">{desc}</p>
+      </div>
       <span className="price">{price}</span>
     </div>
   );
