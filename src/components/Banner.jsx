777ab08100eb18db285dbd8e792c21a1abcf1f0f
LonerStyles <163217333+LonerStyles@users.noreply.github.com>
2026-03-09 00:59:10 -0500

    Home fixes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ export default function Banner() {
   return (
     <section className="banner">
       <div className="banner-box">
-        <h2>VER CARTA</h2>
+        <h2>Ver Carta</h2>
         <h3>NUESTRA PROPUESTA GASTRONÓMICA</h3>
         <button className="btn-primary">Ver Menú</button>
       </div>
